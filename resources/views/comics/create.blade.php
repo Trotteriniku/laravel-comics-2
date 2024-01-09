9d2fcd8a2c57ef7b2e4194bb0d5811ca535320ce
Trotteriniku <stefanozidda3@gmail.com>
2024-01-09 18:01:50 +0100

    edit and create layout fixed with validation

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,31 +1,52 @@
 @extends('layouts.app')
 @section('content')
-    <div class="Myjumbotron"></div>
-    <div class="container-sm bg-primary p-2">
+    <div class="container  bg-primary p-3 rounded-3 " style="margin: 40px auto">
         <form action="{{ route('comics.store') }}" method="POST"enctype="multipart/form-data">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
             @csrf
-            <input type="text" name="title" id="title" placeholder="inserisci titolo"
-                class="form-control text-center input-color ">
-            <input type="text" name="price" id="price" placeholder="price"
-                class="form-control text-center input-color">
+            <input type="text" name="title" id="title" value="{{ old('title') }}" placeholder="inserisci titolo"
+                class="form-control  input-color @error('title') is-invalid @enderror " required>
+            @error('title')
+                <div class="text-light">{{ $message }}</div>
+            @enderror
+            <input type="text" name="price" id="price" placeholder="price" class="form-control  input-color"
+                required>
+
             <input type="text" name="description" id="description"
-                placeholder="description"class="form-control text-center input-color">
-            <input type="text" name="type" id="type" placeholder="type"
-                class="form-control text-center input-color">
-            <input type="text" name="sale_date" id="sale_date" placeholder="sale_date"
-                class="form-control text-center input-color">
-            <input type="text" name="series" id="series" placeholder="series"
-                class="form-control text-center input-color">
+                placeholder="description"class="form-control  input-color" required>
 
-            <div class=" input-group-sm d-flex align-items-center  ">
-                <label for="image" class=" w-25 text-center mt-5  input-color   ">Seleziona
-                    un'immagine:</label>
-                <input type="file" name="thumb" id="thumb" accept="image/*"
-                    class="w-75 text-center input-color h-25 mt-5  ">
-            </div>
 
-            <button type="submit" class="btn btn-primary text-center input-color form-control">invia</button>
+            <label for="description" class="text-light mt-3">Example textarea</label>
+            <textarea class="form-control  input-color" id="description" name="description" placeholder="description"
+                rows="3"></textarea>
+
+            <input type="text" name="type" id="type" placeholder="type" class="form-control  input-color"
+                required>
+
+            <input type="text" name="sale_date" id="sale_date" placeholder="sale_date" class="form-control  input-color"
+                required>
+
+            <input type="text" name="series" id="series" placeholder="series" class="form-control  input-color"
+                required>
+
+
+            <label for="description" class="text-light mt-3  ">Seleziona
+                un'immagine:</label>
+            <input type="url" name="thumb" id="thumb" class=" form-control input-color h-25   "
+                placeholder="inserisci un Url" required>
+
+
+            <button type="submit" class="btn btn-primary  input-color form-control">invia</button>
+            <button type="reset" class="btn btn-primary  input-color form-control">resetta</button>
         </form>
     </div>
 @endsection
---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('title', 'Edit:' . $comic->title)
+
+@section('content')
+    <div class="container bg-primary p-3 rounded-3" style="margin: 40px auto">
+        <form action="{{ route('comics.update', $comic->id) }}" method="POST"enctype="multipart/form-data">
+
+            @csrf
+            @method('PUT')
+            <input type="text" name="title" id="title" placeholder="inserisci titolo" class="form-control  input-color "
+                value="{{ $comic->title }}" required>
+
+            <input type="text" name="price" id="price" placeholder="price" class="form-control  input-color"
+                value="{{ $comic->price }}" required>
+
+            <label for="description" class="text-light mt-3">Example textarea</label>
+            <textarea class="form-control  input-color" id="description" name="description" placeholder="description"
+                rows="3"></textarea>
+
+            <input type="text" name="type" id="type" placeholder="type" class="form-control  input-color"
+                value="{{ $comic->type }}" required>
+
+            <input type="text" name="sale_date" id="sale_date" placeholder="sale_date" class="form-control  input-color"
+                value="{{ $comic->sale_date }}" required>
+
+            <input type="text" name="series" id="series" placeholder="series" class="form-control  input-color"
+                value="{{ $comic->series }}" required>
+
+            <div class=" input-group-sm d-flex align-items-center  ">
+                <label for="description" class=" w-25  mt-5  input-color   ">Seleziona
+                    un'immagine:</label>
+                <input type="url" name="thumb" id="thumb" class="w-75  input-color h-25 mt-5  "
+                    value="{{ $comic->thumb }}" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary  input-color form-control">invia</button>
+            <button type="reset" class="btn btn-primary  input-color form-control">resetta</button>
+        </form>
+    </div>
+@endsection
