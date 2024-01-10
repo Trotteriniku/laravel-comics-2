09f21d29d6e19793953c3118bd995ef44a4c2dca
Trotteriniku <stefanozidda3@gmail.com>
2024-01-10 14:02:41 +0100

    updated validation mode

---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:5|max:255|unique:comics',
+            'description' => 'required',
+            'thumb' => 'url',
+            'price' => 'required|max:20',
+            'sale_date' => 'required',
+            'series' => 'required|max:30',
+            'type' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Il campo titolo è obbligatorio',
+            'title.min' => 'Il campo titolo deve avere almeno :min caratteri',
+            'title.max' => 'Il campo titolo deve avere massimo :max caratteri',
+        ];
+    }
+}
